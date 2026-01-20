5d2ef80f4962b30a59fe16c848930bd4aeaf3a2e
holygrail0063 <akashkamble0063@gmail.com>
2026-01-20 03:31:09 -0500

    Fix build error + pin house header with global header - Fixed CSS syntax error in ChoreTable.css - Made house header fixed/pinned below global header on HousePage and ScheduleOverviewPage - Added CSS variable for house header height - Updated padding to account for fixed house header - House header now stays visible when scrolling on both pages

--- src/pages/ScheduleOverviewPage.tsx
+++ src/pages/ScheduleOverviewPage.tsx
@@ -11,6 +11,7 @@ import {
   startOfWeekMonday 
 } from '../utils/weekUtils';
 import { getCommonAssignmentsForWeek, getSoleResponsibilityAssignmentForWeek } from '../utils/taskAssignment';
+import HouseHeader from '../components/HouseHeader';
 import './ScheduleOverviewPage.css';
 
 export default function ScheduleOverviewPage() {
